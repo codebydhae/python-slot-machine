2c401df2fb1558b646229d3b372a4cfd1c68f69b
“codebydhae” <dhaesha3000@gmail.com>
2024-12-28 21:52:47 -0500

    user input balance and bet option. Program gets structured dynamically

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#global constant
+MAX_LINES  = 3
+
 #collect user input
 def deposit():
     #continue  to ask for input until user enters a valid number
@@ -15,5 +18,24 @@ def deposit():
         else:
             print("Please enter a valid number")
     return amount
-    
 
+def get_number_of_lines():
+    #pick int between 1  and 3
+    while True:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) +  ")? " )
+        if   lines.isdigit():
+            lines  = int(lines)
+            if 1  <=  lines <= MAX_LINES:
+                break
+            else:
+                print("Please enter a number between 1 and 3")
+        else:
+            print("Please enter a valid number")
+    return lines
+
+def main():    
+    balance  = deposit()
+    lines = get_number_of_lines()
+    print(balance, lines)
+
+main()
